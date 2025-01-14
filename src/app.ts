724741d33b6c874c064db3eed9bb865a9e3a09e3
Diljot Singh Sangha <dsangha2@academic.rrc.ca>
2025-01-14 10:07:19 -0600

    feat: integrate Swagger for API documentation

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express, {Express} from "express";
+import setupSwagger from "../config/swagger";
 
 const app: Express = express();
 
+setupSwagger(app);
+
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Respond with "Hello, World!"
+ *     responses:
+ *       200:
+ *         description: A successful response
+ */
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
