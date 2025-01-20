4891ced6de79415e1d46518ee933226164d240b1
Diljot Singh Sangha <dsangha2@academic.rrc.ca>
2025-01-19 23:33:03 -0600

    feat: add findLargestHolding function

---
+++ src/portfolio/assetManagement.ts
@@ -0,0 +1,16 @@
+interface Asset {
+    name: string;
+    value: number;
+}
+
+export function findLargestHolding(assets: Asset[]): Asset | null {
+    let largestAsset: Asset | null = null;
+
+    for (const asset of assets) {
+        if (!largestAsset || asset.value > largestAsset.value) {
+            largestAsset = asset;
+        }
+    }
+
+    return largestAsset;
+}
